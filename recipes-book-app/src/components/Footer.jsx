9e8db89baa769818fcc3021d55065a1d968f90ab
Gara Luzardo <garaluzardo@gmail.com>
2025-02-10 23:11:03 +0000

    Logo del footer linkeado al repositorio de GitHub

--- recipes-book-app/src/components/Footer.jsx
+++ recipes-book-app/src/components/Footer.jsx
@@ -5,7 +5,9 @@ import githubLogo from "../assets/github-logo.png";
 export default function Footer() {
     return (
         <footer className="footer">
+            <a href="https://github.com/garaluzardo/react-app" target="_blank">
             <img src={githubLogo} alt="github logo"></img>
+            </a>
         </footer>
     );
 };
